27193a45ed798411eec16e889d2748291f5da394
boyraf <raphael.kymotho@gmail.com>
2023-05-31 11:27:56 +0300

    learning alembic

---
+++ migrations/versions/c0a20a382759_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: c0a20a382759
+Revises: 
+Create Date: 2023-05-31 11:01:49.074882
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c0a20a382759'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
